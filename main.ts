93bea87b2f3ec8cb7444a8047b27561a08f31a6e
fit-s-u-m <nehemiahmaths@gmail.com>
2024-08-11 17:12:38 +0300

    adding restart button and making it dafault mute

--- main.ts
+++ main.ts
@@ -30,9 +30,27 @@ const sketch = (p: p5) => {
     sortingAlgorithm.option("InsertionSort")
     sortingAlgorithm.selected("BubbleSort")
 
+    const restart = p.createButton("restart")
+    restart.mousePressed(() => {
+      bubbleSort = Sort.SortWith(sortingAlgorithm.selected())
+      iterator = bubbleSort.sort(createArrayForLetters(letters.Amharic)) // create array of numbers from the letters
+      nextIteration = iterator.next()
+    })
+    restart.position(0, 25)
+    restart.size(100)
+    restart.style("border-radius", "8px")
+    restart.mouseOver(() => {
+      restart.style("background-color", "#00f")
+    })
+    restart.mouseOut(() => {
+      restart.style("background-color", "#fff")
+    })
+
     const mute = p.createButton("mute")
     mute.position(0, 0)
     mute.size(100)
+    mute.style("background-color", "#f00") // default muted
+    mute.style("border-radius", "8px")
     mute.mousePressed(() => {
       isMutted = !isMutted
       if (isMutted) {
